5d9b0f57542be5997ae29cea4233e01c7bf449eb
Daniya Niazi <farihadania@hotmail.com>
2021-05-28 23:31:42 +0500

    hook custom component with formik

--- src/components/Regform.js
+++ src/components/Regform.js
@@ -6,6 +6,8 @@ const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
 };
 const onSubmit = (values) => {
   console.log("FORM VALUES", values);
@@ -33,15 +35,39 @@ function Regform() {
         <label htmlFor="name">Name</label>
         <Field type="text" id="name" name="name" />
         <ErrorMessage name="name" />
-
         <label htmlFor="emial">Email</label>
         <Field type="email" id="email" name="email" />
         <ErrorMessage name="email" />
-
         <label htmlFor="Channel">Channel</label>
         <Field type="text" id="channel" name="channel" />
         <ErrorMessage name="channel" />
+        <label htmlFor="comments">Comments</label>
 
+        <Field
+          type="text"
+          id="comments"
+          name="comments"
+          // as = component(deprecated)
+          as="textarea"
+          placeholder="Type your message here"
+        />
+
+        <Field name="address">
+          {/* props */}
+          {(props) => {
+            const { field, form, meta } = props;
+            //field: handle change, blur , name, value
+            // Meta : touched, error?
+            console.log("Rendering Props", props);
+            return (
+              <div>
+                <input type="text" id="address" {...field} />;
+                {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+              </div>
+            );
+          }}
+        </Field>
+        <ErrorMessage name="channel" />
         <button type="submit">Submit</button>
       </Form>
     </Formik>
